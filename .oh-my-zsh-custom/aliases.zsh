19323bd8eb1413876037d3f0b041b71fecd43c08
Alex Glaze <aglaze@joinhandshake.com>
2022-08-20 12:42:09 -0500

    add aliases

---
+++ .oh-my-zsh-custom/aliases.zsh
@@ -0,0 +1,44 @@
+# Navigation
+# alias dcom='cd /Users/chaseschachenman/Projects/dot-com'
+# alias rater='cd /Users/chaseschachenman/Projects/rater-api'
+alias ls='ls -alh '
+
+# Rails
+alias be='bundle exec'
+alias cop='bundle exec rubocop -D --parallel'
+alias kill_rails='pkill -9 -f puma'
+
+# JavaScript
+
+# Emacs
+
+# Heroku Console
+   
+
+# AWS Cli
+
+
+# Terraform
+
+
+# DB
+
+
+# Git
+alias gp='git pull origin'
+alias gad='git add'
+alias gcom='git commit -m'
+alias gpu='git push origin'
+alias gc='git checkout'
+alias gcb='git checkout -b'
+alias gb='git branch'
+alias greb='git rebase -i'
+alias grabrt='git rebase --abort'
+
+# Misc.
+
+
+# Logs
+
+
+# Golang
